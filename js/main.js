3d26197b414d2f90a9d7b5a9ef30cc5bb59f575c
Al1exhawk <aldevid9@gmail.com>
2019-07-01 11:00:39 +0300

    Added filter

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $(".button[filter]").click(function() {
+    if ($(this).attr("val") === "off") {
+      $(".button[filter]").attr("val", "off");
+      $(this).attr("val", "on");
+      if ($(this).attr("filter") === "all") {
+        $(".filter > div").show(300);
+      } else {
+        const filter = $(this).attr("filter");
+        $(".filter > div").hide(300);
+        $(`.filter > div[filter=${filter}]`).show(300);
+      }
+    }
+  });
+});
