b17d2fa44899d5409c8bbeec4d3bde0cd350fa59
Al1exhawk <aldevid9@gmail.com>
2019-07-02 15:11:37 +0300

    Pricing Section added

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     if ($(this).attr("val") === "off") {
       $(".button[filter]").attr("val", "off");
       $(this).attr("val", "on");
+      $(".button[filter]").removeClass("focused");
+      $(this).addClass("focused");
       if ($(this).attr("filter") === "all") {
         $(".filter > div").show(300);
       } else {
